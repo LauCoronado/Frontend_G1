7d225b9488e53c1aabe7a08f8255b664636a3f3b
LauCoronado <laucoronado497@gmail.com>
2022-12-21 20:05:17 -0500

    frontend_G1

---
+++ src/commons/urls.js
@@ -0,0 +1,4 @@
+const urlbase= "http://localhost:8080/";
+export const getAllProductos = urlbase + "api/productos"
+export const getAllInventarios = urlbase + "api/inventarios"
+export const postNewProduct = urlbase + "api/inventarios"
